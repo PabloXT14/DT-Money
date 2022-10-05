8e01160ac926c2eb8e1a5fa6d4bd552de0a8ceea
Pablo Alan <pabloxt14@gmail.com>
2022-10-05 11:00:20 -0300

    feat: created components TransactionsTable, SearchForm and Modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import * as Dialog from '@radix-ui/react-dialog'
 import logoImg from '../../assets/logo.svg'
 import * as S from './styles'
 
@@ -7,7 +8,23 @@ export function Header() {
       <S.HeaderContent>
         <img src={logoImg} alt="" />
 
-        <S.NewTransactionButton>Nova transação</S.NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>{/* asChild informa ao Trigger para utilizar o componente <button> filho como base de estilização, e não precisar criar um <button> dentro de outro <button>*/}
+            <S.NewTransactionButton>Nova transação</S.NewTransactionButton>
+          </Dialog.Trigger>
+
+          {/* Para colocar o nosso modal separado da lógica do Header, fazendo com que fique seja inserido até mesmo fora da div#root, facilitando o entendimento para os leitores de tela */}
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title>Nova transação</Dialog.Title>
+              <Dialog.Description>
+                Content of the modal
+              </Dialog.Description>
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </S.HeaderContent>
     </S.HeaderContainer>
   )
--- src/pages/Transactions/styles.ts
+++ src/pages/Transactions/styles.ts
@@ -1,1 +1,39 @@
 import styled from "styled-components";
+
+export const TransactionsContainer = styled.main`
+  width: 100%;
+  max-width: 1120px;
+  margin: 4rem auto 0;
+  padding: 0 1.5rem;
+
+`;
+
+export const TransactionsTable = styled.table`
+  width: 100%;
+  border-collapse: separate;// cada celula da table tera a borda individual
+  border-spacing: 0 0.5rem;// espaçamente a partir da borda entre as células da tabela (horizontal, vertical)
+  margin-top: 1.5rem;
+
+  td {
+    padding: 1.25rem 2rem;
+    background: ${({ theme }) => theme["gray-700"]};
+
+    &:first-child {
+      border-top-left-radius: 6px;
+      border-bottom-left-radius: 6px;
+    }
+
+    &:last-child {
+      border-top-right-radius: 6px;
+      border-bottom-right-radius: 6px;
+    }
+  }
+`
+
+interface PriceHighlightProps {
+  variant: 'income' | 'outcome';
+}
+
+export const PriceHighlight = styled.span<PriceHighlightProps>`
+  color: ${({ variant, theme }) => variant === 'income' ? theme["green-300"] : theme["red-300"]};
+`
