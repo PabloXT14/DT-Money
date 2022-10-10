699c90c7208217b8c0e5a21d39d2c41fd05d7602
Pablo Alan <pabloxt14@gmail.com>
2022-10-10 11:16:38 -0300

    feat: implemented summary transactions logic

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -10,6 +10,22 @@ export function Summary() {
 
   const { transactions } = useContext(TransactionsContext);
 
+  const summary = transactions.reduce((acc, transaction) => {
+    if (transaction.type === "income") {
+      acc.income += transaction.price;
+      acc.total += transaction.price;
+    } else {
+      acc.outcome += transaction.price;
+      acc.total -= transaction.price;
+    }
+
+    return acc
+  }, {
+    income: 0,
+    outcome: 0,
+    total: 0,
+  });
+
   console.log(transactions);
 
   return (
@@ -20,7 +36,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color={theme['green-300']} />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>R$ {summary.income}</strong>
       </S.SummaryCard>
 
       <S.SummaryCard>
@@ -29,7 +45,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color={theme['red-300']} />
         </header>
 
-        <strong>R$ 1.259,00</strong>
+        <strong>R$ {summary.outcome}</strong>
       </S.SummaryCard>
 
       <S.SummaryCard variant="green">
@@ -38,7 +54,7 @@ export function Summary() {
           <CurrencyDollar size={32} color={theme.white} />
         </header>
 
-        <strong>R$ 16.141,00</strong>
+        <strong>R$ {summary.total}</strong>
       </S.SummaryCard>
     </S.SummaryContainer>
   )
